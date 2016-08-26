aa6eaf8ead2a8f0f40bba97589f23801f36c705e
Deny1990 <yuanxin@yuanxindeMacBook-Pro.local>
2016-08-26 14:10:19 +0800

    test
    controllerTest

---
+++ src/main/java/yuan/spring/boot/test/myTest/ControllerTest.java
@@ -0,0 +1,15 @@
+package yuan.spring.boot.test.myTest;
+
+import com.sun.org.glassfish.external.statistics.annotations.Reset;
+import groovy.lang.Singleton;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by yuanxin on 16/8/26.
+ */
+
+public class ControllerTest {
+
+
+}
